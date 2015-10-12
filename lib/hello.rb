e8cc49dba91b7bcb2531a8ee8980cf22d72e1dfe
cicio <m (at) navegawireless.com>
2015-10-12 04:58:34 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,6 @@
+require 'greeter'
 # Default is "World"
 # Author: Cicio Ingracia (cicio@mymail.com)
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
